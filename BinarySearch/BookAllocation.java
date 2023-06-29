3548fd138e6be746ebc3b20cc3983fba022b6ee7
PampariNaresh <pamparinaresh2001@gmail.com>
2023-06-29 15:37:28 +0530

    Book Allocation Solved

--- BinarySearch/BookAllocation.java
+++ BinarySearch/BookAllocation.java
@@ -38,7 +38,7 @@ class BookAllocation {
 
     public static void main(String args[]) {
         /*
-         * Problem:
+         * Problem Statement:
          * INPUT:
          * Number of Pages in i th book in arr[i]
          * Number of Students -m
