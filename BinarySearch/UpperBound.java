ab3d361183c2b98697c3e0a7c546f1b90b2b0a4a
PampariNaresh <pamparinaresh2001@gmail.com>
2023-06-30 22:14:05 +0530

    UpperBound Solved

---
+++ BinarySearch/UpperBound.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class UpperBound {
+
+    public static void main(String args[]) {
+        int arr[] = { 2, 3, 4, 5, 9, 5, 10, 4, 5, 5 };
+        Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.print(i + "  ");
+        }
+        System.out.println();
+        int key = 5;
+        int low = 0;
+        int ans = arr.length;
+        int high = arr.length - 1;
+        int mid = low + (high - low) / 2;
+        while (low <= high) {
+            if (arr[mid] > key) {
+                ans = mid;
+                high = mid - 1;
+            }
+
+            // If X is greater arr[mid]
+            // then find in right subarray
+            else {
+                low = mid + 1;
+            }
+            mid = low + (high - low) / 2;
+        }
+
+        System.out.println(ans);
+    }
+}
