e3773fd3ad87b0b351ee0bed13254a659a29a77b
PampariNaresh <pamparinaresh2001@gmail.com>
2023-06-30 22:21:23 +0530

    Floor Solved

---
+++ BinarySearch/Floor.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+
+public class Floor {
+    public static void main(String args[]) {
+        int arr[] = { 2, 3, 4, 5, 9, 5, 10, 4, 5, 5 };
+        Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.print(i + "  ");
+        }
+        System.out.println();
+        int key = 8;
+        int low = 0;
+        int ans = -1;
+        int high = arr.length - 1;
+        int mid = low + (high - low) / 2;
+        while (low <= high) {
+            if (arr[mid] <= key) {
+                ans = arr[mid];
+                low = mid + 1;
+            }
+
+            else {
+                high = mid - 1;
+            }
+            mid = low + (high - low) / 2;
+        }
+
+        System.out.println(ans);
+    }
+}
