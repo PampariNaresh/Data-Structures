128fa160486851c30800f51a96e7d402475e9986
PampariNaresh <pamparinaresh2001@gmail.com>
2023-06-30 22:07:39 +0530

    LowerBound Solved

---
+++ BinarySearch/LowerBound.java
@@ -0,0 +1,26 @@
+public class LowerBound {
+
+    public static void main(String args[]) {
+        int arr[] = { 2, 3, 9, 5, 10, 4, 5, 5 };
+        int key = 5;
+        int low = 0;
+        int ans = arr.length;
+        int high = arr.length - 1;
+        int mid = low + (high - low) / 2;
+        while (low <= high) {
+            if (arr[mid] >= key) {
+                ans = mid;
+                high = mid - 1;
+            }
+
+            // If X is greater arr[mid]
+            // then find in right subarray
+            else {
+                low = mid + 1;
+            }
+            mid = low + (high - low) / 2;
+        }
+
+        System.out.println(ans);
+    }
+}
