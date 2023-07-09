0856e63442dcdc6c04b8aa963eac57251bd9934f
PampariNaresh <pamparinaresh2001@gmail.com>
2023-07-09 11:11:16 +0530

    Stack Implementation

---
+++ Stack/Stack.java
@@ -0,0 +1,65 @@
+public class Stack {
+    public int top;
+    public int Stack[];
+    public int size;
+
+    Stack(int size) {
+        this.size = size;
+        Stack = new int[size];
+        top = -1;
+
+    }
+
+    public void push(int element) {
+        if (size - top < 1) {
+            System.out.println("Stack Overflow");
+        } else {
+            top++;
+            Stack[top] = element;
+
+        }
+    }
+
+    public int pop() {
+        if (top >= 0) {
+            int element = Stack[top];
+            top--;
+            return element;
+        } else {
+            System.out.println("Stack Overflow");
+
+        }
+        return -1;
+    }
+
+    public int peek() {
+        if (top >= 0) {
+            return Stack[top];
+        } else {
+            System.out.println("Stack is Empty");
+            return -1;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String args[]) {
+        Stack s = new Stack(5);
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.peek());
+        System.out.println(s.isEmpty());
+
+    }
+}
---
+++ Stack/StackClass.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class StackClass {
+    public static void main(String args[]) {
+        Stack<Integer> s = new Stack<>();
+        s.push(3);
+        s.push(2);
+        s.push(1);
+        System.out.println(s);
+        System.out.println(s.peek());
+        System.out.println(s.empty());
+        System.out.println(s.isEmpty());
+        System.out.println(s.pop());
+        System.out.println(s.search(3));
+        System.out.println(s.capacity());
+        System.out.println(s.size());
+
+    }
+}
